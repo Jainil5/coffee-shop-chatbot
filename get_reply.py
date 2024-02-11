080688b67f8203998169db42252e6e5ed2b9a9bb
effyAI <jainiljp72525@gmail.com>
2024-02-11 13:59:32 +0530

    Internal changes

--- get_reply.py
+++ get_reply.py
@@ -12,7 +12,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
+FILE = "/data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]

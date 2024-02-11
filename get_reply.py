67811aabdd1f981e885383f9850d928a227928d2
effyAI <jainiljp72525@gmail.com>
2024-02-11 14:16:11 +0530

    Internal changes

--- get_reply.py
+++ get_reply.py
@@ -2,18 +2,19 @@ import random
 import json
 import torch
 from model import NeuralNet
-# from spacy_utils import bag_of_words, tokenize
-from nltk_utils import bag_of_words, tokenize
+from spacy_utils import bag_of_words, tokenize
+# from nltk_utils import bag_of_words, tokenize
 from items_list import items 
 from get_order import order
+from train import data
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-FILE = "data.pth"
-data = torch.load(FILE)
+# FILE = "data.pth"
+# data = torch.load(FILE)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
@@ -22,7 +23,14 @@ all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
 
-print(input_size,hidden_size,output_size,all_words,tags,model_state)
+# print(input_size,hidden_size,output_size,all_words,tags,model_state)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
